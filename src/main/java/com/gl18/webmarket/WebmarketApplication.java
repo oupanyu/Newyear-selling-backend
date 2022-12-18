a701bb19b2e3e0d20bb9a0bb269a1abda8c8a08f
oupanyu <oupanyu615@hotmail.com>
2022-12-18 16:00:36 +0800

    Project Done!

--- src/main/java/com/gl18/webmarket/WebmarketApplication.java
+++ src/main/java/com/gl18/webmarket/WebmarketApplication.java
@@ -2,8 +2,9 @@ package com.gl18.webmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebmarketApplication {
@@ -18,4 +19,24 @@ public class WebmarketApplication {
     }
 
 
+    //springboot 2.0以上的方式
+    @Configuration
+    public static class WebMvcConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    //是否发送Cookie
+                    .allowCredentials(true)
+                    //设置放行哪些原始域   SpringBoot2.4.4下低版本使用.allowedOrigins("*")
+                    .allowedOriginPatterns("*")
+                    //放行哪些请求方式
+                    .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+                    //.allowedMethods("*") //或者放行全部
+                    //放行哪些原始请求头部信息
+                    .allowedHeaders("*")
+                    //暴露哪些原始请求头部信息
+                    .exposedHeaders("*");
+        }
+    }
 }
